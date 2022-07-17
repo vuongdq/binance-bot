48f6a66a19c2df72c6165000d298646ed3a59ffb
vuongdq <quocvuong_it@yahoo.com>
2022-07-17 16:59:18 +0700

    Live Trading -- binance1.js and Testnet Trading --- index.js

--- binance1.js
+++ binance1.js
@@ -3,21 +3,18 @@ const moment = require("moment");
 const delay = require("delay");
 
 const binance = new ccxt.binance({
-    apiKey: 'TkDtH8KO4WhgbPtfnCSikFSC7OCl6rR4WmB7lbL3Ya1gXdJmDDHLZkZxNHs2Dqce',
-    secret: 'ubi9o4yEa3L3F4zIfzxbZCJAnPrLkU4OHiTfobOUXFhWtAjj6XOcYvLMPRufzXNn'
+    apiKey: 'YiWWLLIyvt4ugJnuEo8UDHpiWrCReh6LQVMzrOKGeJgje4CUmV6CkO79HLeKZ6j3sa',
+    secret: 'Ws3OaZtsFNYwKHmoLPIr7vSdlkfVZwdkqVuv4wltFqQgZXMykaKM5pwhGhAhRC25'
 });
- binance.setSandboxMode (true);
-
 
 async function printBalance(btcPrice){
     const balance = await binance.fetchBalance();
     const total = balance.total
     console.log(`Balance: BTC ${total.BTC}, USDT ${total.USDT}`);
-    console.log(`Total USD: ${(total.BTC-1)*btcPrice +total.USDT}. \n`);
+    console.log(`Total USD: ${(total.BTC)*btcPrice +total.USDT}. \n`);
 }
 
-async function tick() {
-
+async function main(){
     const price = await binance.fetchOHLCV('BTC/USDT','1m',undefined,5);
     //console.log(price);
     const bPrice = price.map(price=>{
@@ -37,32 +34,15 @@ async function tick() {
     console.log(bPrice.map(p=>p.close),avaragePrice,lastPrice);
     console.log("--ket thuc Thong tin gia--")
 
-    // Tinh toan trade
-    // if(lastPrice>avaragePrice){
-    //     let direction = 'sell'
-    // }else {
-    //     let direction ='buy'
-    // }
-    // Dung Ternary cho chuyen nghiep
-    const direction = lastPrice>avaragePrice ? 'sell': 'buy';
-    const TRADE_SIZE = 1100;
-    const quantity = 1100/lastPrice;
+    const direction = 'buy';
+    const TRADE_SIZE = 40;
+    const quantity = 40/lastPrice;
 
     // tao giao dich
     console.log("--Thong tin gia--")
     console.log('avarage price:'+ avaragePrice+ '------last price: '+ lastPrice); // in thong tin price
-    const order = await binance.createMarketOrder('BTC/USDT',direction,quantity);
+     const order = await binance.createLimitBuyOrder("BTC/USDT",quantity,19000);
     console.log(`${moment().format()}:${direction}${quantity} BTC at ${lastPrice}`);
     printBalance(lastPrice);
-    // console.log(order);
 }
-
-async function main(){
-    while (true){
-        await tick();
-        await delay(60*1000)
-    }
-}
-
 main();
-// printbalance()
